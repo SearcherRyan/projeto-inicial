6feb228fba671657e5a3374364da14b956bde652
SearcherRyan <ryan.2007palmeiras@gmail.com>
2024-03-15 08:23:16 -0300

    variavel nome

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var botao = document.getElementById("botao")
 botao.addEventListener("click", alertar, false);
-
+var nome = document.getElementById("nome");
 function alertar(event){
-    alert("Você clicou no botão!!! " + event);
+    alert("Você clicou no botão!!! " + "" +nome.value);
+var numero = 2;
+var resul = numero % 2;
+if(resul == 0){
+    alert("Numero par !!!: "+ numero);
 }
-
+}
