239ed1fd48d8c0d093dc3755b31e556c5dec2582
Searcher Ryan <ryan.2007palmeiras@gmail.com>
2024-03-20 09:58:25 -0300

    formulario

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 var botao = document.getElementById("botao")
 botao.addEventListener("click", alertar, false);
+
 var nome = document.getElementById("nome");
+var email = document.getElementById("email");
+var saida = document.getElementById("saida-de-dados");
+var telefone = document.getElementById("telefone");
+var CEP = document.getElementById("CEP");
+var logra = document.getElementById("logra");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
 function alertar(event){
-    alert("Você clicou no botão!!! " + "" +nome.value);
-var numero = 2;
-var resul = numero % 2;
-if(resul == 0){
-    alert("Numero par !!!: "+ numero);
-}
+
+saida.innerText = "nome: " +nome.value 
++"\n Email: " +email.value 
++ "\n Telefone: " + telefone.value
++ "\n CEP: " + CEP.value
++ "\n Logradouro: " + logra.value
++ "\n Bairro: " + bairro.value
++ "\n Cidade: " + cidade.value
++ "\n Estado: " + estado.value;
 }
