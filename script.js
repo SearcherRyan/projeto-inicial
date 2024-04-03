6bbb60739cca9354ad32f3610f592e8500e56e9d
Searcher Ryan <ryan.2007palmeiras@gmail.com>
2024-04-03 09:42:45 -0300

    inserir api e bloquear modificacao dos eementos

--- script.js
+++ script.js
@@ -10,12 +10,33 @@ var logra = document.getElementById("logra");
 var bairro = document.getElementById("bairro");
 var cidade = document.getElementById("cidade");
 var estado = document.getElementById("estado");
+
 function alertar(event){
+    const url = `https://viacep.com.br/ws/${CEP.value}/json`;
+
+fetch(url).then
+(function(resposta){
+    return resposta.json();
+})
+.then(
+    function(dadosDoEndereco){
+        logra.value = dadosDoEndereco.logradouro;
+        bairro.value = dadosDoEndereco.bairro;
+        cidade.value = dadosDoEndereco.localidade;
+        estado.value = dadosDoEndereco.uf;
+        complemento.value = dadosDoEndereco.complemento;
+    }
+)
+.catch(function(e){
+    alert(e.message());
+});
 
 saida.innerText = "nome: " +nome.value 
 +"\n Email: " +email.value 
 + "\n Telefone: " + telefone.value
 + "\n CEP: " + CEP.value
++ "\n Numero: " + numero.value
++ "\n Complemento: " + comp.value
 + "\n Logradouro: " + logra.value
 + "\n Bairro: " + bairro.value
 + "\n Cidade: " + cidade.value
